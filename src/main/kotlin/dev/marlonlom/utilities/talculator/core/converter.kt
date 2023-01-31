84e18e48387ccd2dd8c2456ce19b86bc2c8e70e0
Marlon Jose Lopez Meza <malm.gm@gmail.com>
2023-01-31 09:47:54 -0500

    feat: added javadoc

--- src/main/kotlin/dev/marlonlom/utilities/talculator/core/converter.kt
+++ src/main/kotlin/dev/marlonlom/utilities/talculator/core/converter.kt
@@ -1,46 +1,89 @@
 package dev.marlonlom.utilities.talculator.core
 
+/**
+ * Data class for temperature conversion request.
+ * @author marlonlom
+ *
+ * @constructor Constructs data class.
+ *
+ *  @property valueToConvert temperature numeric value to be converted.
+ *  @property temperatureUnit temperature unit enum value to be used.
+ */
 data class TemperatureConvertRequest(
-  val valueToConvert: Double,
-  val temperatureUnit: TemperatureUnit
+  val valueToConvert: Double, val temperatureUnit: TemperatureUnit
 )
 
+/**
+ * Data class for temperature conversion response.
+ * @author marlonlom
+ *
+ * @constructor Constructs data class.
+ *
+ *  @property celsiusValue celsius response value.
+ *  @property fahrenheitValue fahrenheit response value.
+ *  @property kelvinValue kelvin response value.
+ *  @property rankineValue rankine response value.
+ */
 data class TemperatureConvertResponse(
-  val celsiusValue: Double,
-  val fahrenheitValue: Double,
-  val kelvinValue: Double,
-  val rankineValue: Double
+  val celsiusValue: Double, val fahrenheitValue: Double, val kelvinValue: Double, val rankineValue: Double
 ) {
 
-  fun isBoilingPointOfWater(): Boolean = celsiusValue.equals(100.0)
-    .and(fahrenheitValue.equals(212.00))
-    .and(kelvinValue.equals(373.15))
-    .and(rankineValue.equals(671.67))
+  /**
+   * Returns if temperature is boiling point of water.
+   * @return true/false.
+   */
+  fun isBoilingPointOfWater(): Boolean =
+    celsiusValue.equals(100.0).and(fahrenheitValue.equals(212.00)).and(kelvinValue.equals(373.15))
+      .and(rankineValue.equals(671.67))
 
-  fun isFreezingPointOfWater(): Boolean = celsiusValue.equals(0.0)
-    .and(fahrenheitValue.equals(32.00))
-    .and(kelvinValue.equals(273.15))
-    .and(rankineValue.equals(491.67))
+  /**
+   * Returns if temperature is freezing point of water.
+   * @return true/false.
+   */
+  fun isFreezingPointOfWater(): Boolean =
+    celsiusValue.equals(0.0).and(fahrenheitValue.equals(32.00)).and(kelvinValue.equals(273.15))
+      .and(rankineValue.equals(491.67))
 
-  fun isAbsoluteZero(): Boolean = celsiusValue.equals(-273.15)
-    .and(fahrenheitValue.equals(-459.67))
-    .and(kelvinValue.equals(0.0))
-    .and(rankineValue.equals(0.0))
+  /**
+   * Returns if temperature is absolute zero.
+   * @return true/false.
+   */
+  fun isAbsoluteZero(): Boolean =
+    celsiusValue.equals(-273.15).and(fahrenheitValue.equals(-459.67)).and(kelvinValue.equals(0.0))
+      .and(rankineValue.equals(0.0))
 
-  fun isRoomTemperature(): Boolean = celsiusValue.equals(21.00)
-    .and(fahrenheitValue.equals(69.8))
-    .and(kelvinValue.equals(294.15))
-    .and(rankineValue.equals(529.47))
+  /**
+   * Returns if temperature is room temperature.
+   * @return true/false.
+   */
+  fun isRoomTemperature(): Boolean =
+    celsiusValue.equals(21.00).and(fahrenheitValue.equals(69.8)).and(kelvinValue.equals(294.15))
+      .and(rankineValue.equals(529.47))
 
-  fun isAverageBodyTemperature(): Boolean = celsiusValue.equals(37.00)
-    .and(fahrenheitValue.equals(98.6))
-    .and(kelvinValue.equals(310.15))
-    .and(rankineValue.equals(558.27))
+  /**
+   * Returns if temperature is average body.
+   * @return true/false.
+   */
+  fun isAverageBodyTemperature(): Boolean =
+    celsiusValue.equals(37.00).and(fahrenheitValue.equals(98.6)).and(kelvinValue.equals(310.15))
+      .and(rankineValue.equals(558.27))
 }
 
+/**
+ * Main class for temperature converter utility.
+ * @author marlonlom
+ *
+ * @constructor Constructs utility class.
+ */
 class TemperatureConverter {
-  fun calculate(request: TemperatureConvertRequest): TemperatureConvertResponse =
-    request.let {
-      it.temperatureUnit.getConverter().calculate(it.valueToConvert)
-    }
+  /**
+   * Performs temperature conversion calculation using the request.
+   * @param request conversion request.
+   * @return conversion response, that contains selected units.
+   */
+  fun calculate(
+    request: TemperatureConvertRequest
+  ): TemperatureConvertResponse = request.let {
+    it.temperatureUnit.getConverter().calculate(it.valueToConvert)
+  }
 }
