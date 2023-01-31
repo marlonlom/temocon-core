23f11fdcc00ae9f66bd4942dd7f1a70c33932e16
Marlon Jose Lopez Meza <malm.gm@gmail.com>
2023-01-31 01:04:01 -0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin("jvm") version "1.7.10"
+}
+
+group = "dev.marlonlom.utilities"
+version = "1.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation(kotlin("test"))
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "1.8"
+}
