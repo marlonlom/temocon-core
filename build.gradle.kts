0fe0fde14d2da8485ade05a318118adc59dd36bb
Marlon Jose Lopez Meza <malm.gm@gmail.com>
2023-02-03 16:28:14 -0500

    feat: updated license header text

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2023 Marlonlom
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ *
+*/
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
+/*
+ * Copyright 2023 Marlonlom
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ *
+*/
+
 rootProject.name = "talculator-core"
